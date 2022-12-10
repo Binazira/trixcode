87660a3acf2d5c93679905e965e4a604f93b27b7
Binazira <binazir.usenkanova@gmail.com>
2022-12-10 14:26:22 +0600

    header responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,22 +2,49 @@ import React from 'react';
 import logo from "../../assets/icons/logo.svg"
 import user from "../../assets/icons/user.svg"
 import humburger from "../../assets/icons/humburger.svg"
+import arrow from "../../assets/icons/arrow.svg"
 import "./style.scss"
 
 const Header = () => {
     return (
         <header className='header'>
-            <div className='header__logo'>
+            <div className='header__logo mobile'>
                 <img className='logo' src={logo} alt="trixcode" />
             </div>
-            <div className='header__user'>
+            <div className='header__user mobile'>
                 <a href="#">
                     <img src={user} alt="user" />
                 </a>
             </div>
-            <div className='header__burger'>
+            <div className='header__burger mobile'>
                     <img src={humburger} alt="humburger button" />
             </div>
+
+            <div className="brand">
+
+                <div className='header__logo'>
+                    <img className='logo' src={logo} alt="trixcode" />
+                </div>
+                <a className='brand__item' href="#">individuals</a>
+                <a className='brand__item' href="#">Companies</a>
+
+            </div>
+            
+            <nav className='nav'>
+                <ul className="nav-menu">
+                    <li className="nav-menu__item">Courses 
+                        <img className='dropdown-menu' src={arrow} alt="dropdown-menu" />
+                    </li>
+                    <li className="nav-menu__item">
+                        <a href="#">Log in</a>
+                    </li>
+                    <li className="nav-menu__item">
+                        <button className='nav-menu__btn'>subscribe</button>
+                    </li>
+                </ul>
+            </nav>
+
+
         </header>
     );
 };
